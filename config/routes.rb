88adf5991ab9f9595fc9e090b7bc877849fe8e48
Matthew Greenfield <mattgreenfield1@gmail.com>
2016-08-15 22:07:30 -0700

    Added calendar

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
   root 'home#index'
+
+  get '/calendar', to: 'calendar#index'
   
   resources :workout_transactions
   resources :cardio_definitions
   resources :mma_definitions
   resources :weightlifting_definitions
   resources :workout_definitions
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815015257) do
+ActiveRecord::Schema.define(version: 20160816045340) do
+
+  create_table "calendars", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "workout_definitions", force: :cascade do |t|
     t.string   "name"
